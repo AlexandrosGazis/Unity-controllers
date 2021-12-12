504eaa14fb44dc492667891fc698cd3f3bb4dc07
AlexandrosGazis <38253545+AlexandrosGazis@users.noreply.github.com>
2021-12-12 22:19:09 +0200

    Add files via upload
    Player Controller wasd movement

---
+++ PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    ///public vs private--> private value cannot be edited in unity
+
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+    public float gravityScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        if (controller.isGrounded)
+        {//check if controller is attached
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+        moveDirection.y = moveDirection.y + (gravityScale * Physics.gravity.y);// * Time.deltaTime); //added gravity
+        controller.Move(moveDirection * Time.deltaTime);//move player
+    }
+}
